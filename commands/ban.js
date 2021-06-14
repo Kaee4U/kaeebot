6c7772689d9915a478e0adcda35495a94afc53c9
unknown <mustafid.mk@gmail.com>
2021-06-14 20:17:11 +0700

    Memperbaiki command ban lagi

--- commands/ban.js
+++ commands/ban.js
@@ -1,18 +1,17 @@
 module.exports = {
-    name: 'ban',
-    description: "ban command!",
+    name: 'hai',
+    description: "hai command!",
     execute(message, args){
-        const member = message.mentions.users.first();
-        if(member){
-            const memberTarger = message.guild.members.cache.get(member.id);
-            memberTarger.ban();
-            message.reply("Badut itu telah di ban");
-        }else{
-            message.reply("Kamu gabisa ban badut itu");
-        }
-        var role = message.member.role.cache.find(r => r.name === "CEO")
-        if(!role){
-            return message.reply("Kamu gapunya akses untuk ban");
-        }
-    }
-}
+        if (!message.member.permission.has("Ban Member")) return;
+
+        const member = message.mentions.member.first();
+        if (!member) return message.reply("Tolong mention badutnya");
+
+        if (message.member.roles.highest.position <= member.roles.highes.position) return message.reply ("Role kamu terlalu rendah/sama dengan role yang ingin kamu ban");
+
+        const reason = args.slice(1).join(" ") || "Di ban karena ga tulen";
+
+        member.ban({reason});
+        message.channel.send(`Banned ${member} for ${reason}`)
+    },
+};
