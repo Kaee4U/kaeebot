302f87daa23b4f2498b6e86792386fc4630dc47c
unknown <mustafid.mk@gmail.com>
2021-06-14 19:25:45 +0700

    Memperbaiki command ban dan menambahkan embed

--- commands/help.js
+++ commands/help.js
@@ -10,7 +10,8 @@ module.exports = {
             {name:'Command 1', value:'*hai'},
             {name:'Command 2', value:'*kamuganteng'},
             {name:'Command 3', value:'*paptt'},
-            {name:'Command 4', value:'*ping'}
+            {name:'Command 4', value:'*ping'},
+            {name:'Command 5', value:'*ban'}
         )
         .setImage('https://cdn.discordapp.com/attachments/391767928749686794/853499735122116628/FB_IMG_1623560580121.jpg')
         .setFooter('Sekian')
