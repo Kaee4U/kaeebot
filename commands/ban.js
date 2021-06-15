5f822e87750dc89279fd83426d0d5f13a7c3825a
unknown <mustafid.mk@gmail.com>
2021-06-15 08:21:48 +0700

    mencoba ban command 2

--- commands/ban.js
+++ commands/ban.js
@@ -5,10 +5,14 @@ module.exports = {
     run: async (client, message, args) => {
         if (!message.member.permission.has("BAN_MEMBERS")) return;
 
-        const member = message.mentions.member.first();
+        const member = message.mentions.members.first();
         if (!member) return message.reply("Tolong mention badutnya");
 
-        if (message.member.roles.highest.position <= member.roles.highes.position) return message.reply ("Role kamu terlalu rendah/sama dengan role yang ingin kamu ban");
+        if (
+            message.member.roles.highest.position <= 
+            member.roles.highes.position
+            ) 
+                return message.reply ("Role kamu terlalu rendah/sama dengan role yang ingin kamu ban");
 
         const reason = args.slice(1).join(" ") || "Di ban karena ga tulen";
 
