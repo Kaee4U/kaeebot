a5fe23294667369ef58c7029c04bcb164135bba9
unknown <mustafid.mk@gmail.com>
2021-06-15 07:15:27 +0700

    Penambahan translate ke 2

--- commands/translate.js
+++ commands/translate.js
@@ -2,7 +2,7 @@ const translate = require('translate-google')
 
 module.exports= {
     name : 'translate',
-    run : async(client, message, args) => {
+    execute(client, message, args){
         translate(args.join(" "), {to : 'en'}).then(res => {
             message.channel.send(res)
         }).catch(err => {
