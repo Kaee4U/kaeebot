85d2400edf90a3d6be5869b8861af117f64707be
unknown <mustafid.mk@gmail.com>
2021-06-15 10:13:46 +0700

    final sementara

--- commands/help.js
+++ commands/help.js
@@ -13,6 +13,7 @@ module.exports = {
             {name:'Command 3', value:'*paptt'},
             {name:'Command 4', value:'*ping'},
             {name:'Command 5', value:'*translate'},
+            {name:'Command 6', value:'*ban'}
         )
         .setImage('https://cdn.discordapp.com/attachments/391767928749686794/853499735122116628/FB_IMG_1623560580121.jpg')
         .setFooter('Sekian')
